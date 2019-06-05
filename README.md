ac059b9d933246d61823b634b2c1d7a0f0327d83
Edwin Carrasquillo <edwin.carrasquillo@datadoghq.com>
2019-06-05 02:09:43 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-(PRAW python Reddit API Wrapper)[https://praw.readthedocs.io/en/v3.6.0/]
+# Install Python3 and pip3
+
+(Installing Pytho3)[https://realpython.com/installing-python/]
+Installing Pip3:
+- (Windows)[https://www.liquidweb.com/kb/install-pip-windows/]
+- Mac: `brew install python3` Comes with `pip3` installed.
+
+# Install PRAW
+
+Documentation: (PRAW python Reddit API Wrapper)[https://praw.readthedocs.io/en/v3.6.0/index.html?highlight=install#installation]
+
+Pip Install: `pip3 install praw`
+
+# Run script by running following command:
+
+`python3 collect_and_add.py`
+
+Then answer the questions that are asked.
+
+Examples:
+- Enter the /r/sub_name without the "/r/"
+`Please enter Subname to submit users & hit Enter: OldSchoolCool`
+- Enter full url of sub you want to add users to
+`Enter comment thread url to scrape users from: https://www.reddit.com/r/AskReddit/`
--- collect_and_add.py
+++ collect_and_add.py
@@ -1,80 +1,52 @@
 import time
 from datetime import datetime
 import praw
-
-
-import time
-from datetime import datetime
-import praw
+from praw.models import MoreComments
 
 #++++++++++++ Collect Users From Comments ========+++++++
+
+new_sub_name = input("Please enter Subname to submit users & hit Enter: ")
+comment_thread_url = input("Enter comment thread url to scrape users from: ")
 num = 0
-with open('reddit_users.txt', 'r') as f:
-    author_list = f.readlines()
+
+reddit = praw.Reddit(client_id='<your_user_api_client_id>',
+                 client_secret='<client_secret_code',
+                 user_agent='commentExtracterApp:v0.1.0 (by /u/<reddit_user>)',
+                 username='your_username', password='<your_password>')
+
+new_sub = reddit.subreddit(new_sub_name)
+submission = reddit.submission(url=comment_thread_url)
+author_list = []
+time.sleep(5)
+submission.comments.replace_more(limit=None)
+for top_level_comment in submission.comments:
+    author = top_level_comment.author
+    if not author:
+        continue
+    else:
+        author_list.append(author)
+        print(author)
 for users in author_list:
-    users = users.replace("\n", "")
-    reddit = praw.Reddit(client_id='LWJFQdtre7sMUw',
-                     client_secret='CpLeDHNaUL_AwcUB4-9_v-GIO1M',
-                     user_agent='commentExtracterMomo:v0.1.0 (by /u/edwcarra17)',
-                     username='edwcarra17', password='EE?!01477410ee')
-
-    sub = 'EGirlSafeHaven'
-    try:
-        if num == 20:
-            time.sleep(40)
-            num -= 20
-            continue
-        else:
-            reddit.subreddit(sub).contributor.add(users)
-            num += 1
-            print(num)
-            print("{} added to {}".format(users,sub))
-            f.close()
-            with open("reddit_users.txt", "w") as writeF:
-                for line in lines:
-                    if line.strip("\n") == users:
-                        writeF.write(line)
-                        writeF.close()
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except:
-        for k,v in reddit.auth.limits.items():
-            if k == 'reset_timestamp':
-                ts = int(v)
-                print("Resetting at: " + datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
-        pass
-print("All users from reddit_users.txt added.")
-
-
+    with open("reddit_users.txt", "a") as file:
+        file.write("{}\n".format(users))
+file.close()
 
 #++++++++++++ ADD Users ========+++++++
-num = 0
+
 with open('reddit_users.txt', 'r') as f:
     author_list = f.readlines()
 for users in author_list:
     users = users.replace("\n", "")
-    reddit = praw.Reddit(client_id='LWJFQdtre7sMUw',
-                     client_secret='CpLeDHNaUL_AwcUB4-9_v-GIO1M',
-                     user_agent='commentExtracterMomo:v0.1.0 (by /u/edwcarra17)',
-                     username='edwcarra17', password='EE?!01477410ee')
-
-    sub = 'EGirlSafeHaven'
     try:
         if num == 20:
             time.sleep(40)
             num -= 20
             continue
         else:
-            reddit.subreddit(sub).contributor.add(users)
+            reddit.subreddit(new_sub_name).contributor.add(users)
             num += 1
             print(num)
-            print("{} added to {}".format(users,sub))
-            f.close()
-            with open("reddit_users.txt", "w") as writeF:
-                for line in lines:
-                    if line.strip("\n") == users:
-                        writeF.write(line)
-                        writeF.close()
+            print("{} added to {}".format(users,new_sub_name))
     except (KeyboardInterrupt, SystemExit):
         raise
     except:
